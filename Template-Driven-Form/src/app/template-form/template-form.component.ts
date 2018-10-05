7165466e725439e09ce7149e5a271d8f875b2ddb
gshivammsg <gshivammsg1997@gmail.com>
2018-10-05 17:44:31 +0530

    First Commit

---
+++ Template-Driven-Form/src/app/template-form/template-form.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  log(firstName){
+   console.log(firstName);
+  }
+
+  submit(f){
+    console.log(f);
+  }
+
+}
